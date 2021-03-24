c679bb1c78b7284a27d5d126ac1d790834602f22
rwan <erwan_lemaire@yahoo.fr>
2021-03-24 21:07:25 +0100

    init

---
+++ tabletopaudio.js
@@ -0,0 +1,65 @@
+/**
+ * This is your JavaScript entry file for Foundry VTT.
+ * Register custom settings, sheets, and constants using the Foundry API.
+ * Change this heading to be more descriptive to your module, or remove it.
+ * Author: [your name]
+ * Content License: [copyright and-or license] If using an existing system
+ * 					you may want to put a (link to a) license or copyright
+ * 					notice here (e.g. the OGL).
+ * Software License: [your license] Put your desired license here, which
+ * 					 determines how others may use and modify your module
+ */
+
+// Import JavaScript modules
+import { registerSettings } from "./module/settings.js";
+import { preloadTemplates } from "./module/preloadTemplates.js";
+
+/* ------------------------------------ */
+/* Initialize module					*/
+/* ------------------------------------ */
+Hooks.once("init", async function () {
+  console.log("tabletopaudio | Initializing tabletopaudio");
+
+  // Assign custom classes and constants here
+
+  // Register custom module settings
+  registerSettings();
+
+  // Preload Handlebars templates
+  await preloadTemplates();
+
+  // Register custom sheets (if any)
+});
+
+/* ------------------------------------ */
+/* Setup module							*/
+/* ------------------------------------ */
+Hooks.once("setup", function () {
+  // Do anything after initialization but before
+  // ready
+});
+
+/* ------------------------------------ */
+/* When ready							*/
+/* ------------------------------------ */
+Hooks.once("ready", function () {
+  let button = document.createElement("div");
+
+  button.id = "TTA";
+  buton.innerHTML =
+    '<button style="position:absolute; bottom:0px; right:200px">ouvrir tabletopaudio</button>';
+  let TTA = new FrameViewer(
+    "https://tabletopaudio.com/alien_starship_sp.html",
+    {
+      title: "tabletopaudio",
+    }
+  );
+  button.addEventListener("click", function () {
+    TTA.render(true);
+  });
+  document.body.append(button);
+
+  // Do anything once the module is ready
+});
+
+// Add any additional hooks if necessary
